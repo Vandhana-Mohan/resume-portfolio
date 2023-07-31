403793ae8cb572d80643ae8f27d19ab11e6f4e19
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-07-31 04:03:31 -0400

    dark mode works in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
 
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  function handleThemeSwitch() {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
   return (
-    <div>
-      
+    <div className="h-screen bg-blue-400 dark:bg-black flex justify-center items-center">
+      <button
+        className="bg-blue-600 p-4 rounded-3xl"
+        onClick={handleThemeSwitch}
+      >
+        Dark Mode
+      </button>
     </div>
   );
 }
